1309506102c0d4a69f963e8edea3ae79b6eccb66
tartarotti <tartarotti.felipe@gmail.com>
2019-07-08 15:14:53 -0300

    return user after creations

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -23,7 +23,7 @@ router.post('/register',async (req,res)=>{
         }
      
         const user = User.create(req.body);
-        return res.send(user.id);
+        return res.send(user);
         
     }catch(err){
         return res.status(400).send({error: 'Registration failed'})
