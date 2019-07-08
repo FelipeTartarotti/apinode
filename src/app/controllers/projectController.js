9a729a2c51ce5ed65658277bdc28c39b8ad6b569
tartarotti <tartarotti.felipe@gmail.com>
2019-07-08 15:08:01 -0300

    12456

--- src/app/controllers/projectController.js
+++ src/app/controllers/projectController.js
@@ -93,6 +93,28 @@ router.put('/:projectId/:userId', async (req,res)=>{
     }
 });
 
+router.put('/:projectId/:userId/:deviceId', async (req,res)=>{
+    try{
+        const {devices} = req.body;
+
+        if(req.userId.toString() != req.params.userId.toString()){
+            return res.status(400).send({error: 'Invalid User Id'});
+        }
+
+        const project = await Project.findByIdAndUpdate(
+            req.params.projectId,
+            {$push: { devices: { $in: req.params.deviceId }}});
+
+        await project.save();
+        
+        return res.send({project});
+
+    }catch(err){
+        console.log(err);
+        return res.status(400).send({error: 'Error updating new project'});
+    }
+});
+
 router.delete('/:deviceId/:userId', async (req,res)=>{
   
     try{
@@ -130,3 +152,34 @@ router.delete('/device/:deviceId/:projectId/:userId', async (req,res)=>{
 });
 
 module.exports = app => app.use('/projects',router);
+
+
+
+/*$currentDate 	Sets the value of a field to current date, either as a Date or a Timestamp.
+$inc 	Increments the value of the field by the specified amount.
+$min 	Only updates the field if the specified value is less than the existing field value.
+$max 	Only updates the field if the specified value is greater than the existing field value.
+$mul 	Multiplies the value of the field by the specified amount.
+$rename 	Renames a field.
+$set 	Sets the value of a field in a document.
+$setOnInsert 	Sets the value of a field if an update results in an insert of a document. Has no effect on update operations that modify existing documents.
+$unset 	Removes the specified field from a document.
+
+Update Operators
+Name 	Description
+$ 	Acts as a placeholder to update the first element that matches the query condition.
+$[] 	Acts as a placeholder to update all elements in an array for the documents that match the query condition.
+$[<identifier>] 	Acts as a placeholder to update all elements that match the arrayFilters condition for the documents that match the query condition.
+$addToSet 	Adds elements to an array only if they do not already exist in the set.
+$pop 	Removes the first or last item of an array.
+$pull 	Removes all array elements that match a specified query.
+$push 	Adds an item to an array.
+$pullAll 	Removes all matching values from an array.
+Update Operator Modifiers
+Name 	Description
+$each 	Modifies the $push and $addToSet operators to append multiple items for array updates.
+$position 	Modifies the $push operator to specify the position in the array to add elements.
+$slice 	Modifies the $push operator to limit the size of updated arrays.
+$sort 	Modifies the $push operator to reorder documents stored in an array.
+
+*/
